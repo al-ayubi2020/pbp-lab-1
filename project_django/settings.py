b3eb331573dbd6dc0964163f347ca16b02dd57fc
al-ayubi2020 <al.ayubi2020@gmail.com>
2022-10-05 19:03:20 +0700

    add tailwind

--- project_django/settings.py
+++ project_django/settings.py
@@ -45,7 +45,18 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'example_app',
     'wishlist',
+    'tailwind',
+    'theme',
+    'django_browser_reload'
 ]
+
+TAILWIND_APP_NAME = 'theme'
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
+
+NPM_BIN_PATH = r"C:\Program Files\nodejs\npm.cmd"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -57,6 +68,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    "django_browser_reload.middleware.BrowserReloadMiddleware",
 ]
 
 ROOT_URLCONF = 'project_django.urls'
