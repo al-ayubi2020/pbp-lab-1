f744025faf066e103bdf9227052c39e4e2d504b5
al-ayubi2020 <al.ayubi2020@gmail.com>
2022-10-06 12:57:24 +0700

    move url

--- wishlist/views.py
+++ wishlist/views.py
@@ -76,7 +76,7 @@ def login_user(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user) 
-            response = HttpResponseRedirect(reverse("wishlist:show_wishlist")) 
+            response = HttpResponseRedirect(reverse("wishlist:show_wishlist_ajax")) 
             return response
         else:
             messages.info(request, 'Username atau Password salah!')
